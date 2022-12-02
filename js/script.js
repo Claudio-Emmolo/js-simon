02badbe54613b947ee6e8758550ee8c92cf598d6
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-12-02 17:35:48 +0100

    Add wait 10s

--- js/script.js
+++ js/script.js
@@ -3,19 +3,22 @@ const button = document.getElementById ("btn");
 let contatore = 0;
 
 let lock;
-button.innerHTML = `#${contatore}`;
 
+button.innerHTML = `#${contatore}`;
 
 button.addEventListener('click', function(){
     lock = false
-    if (lock){
-        return;
-    }
-    
+
     if (contatore === 10){
         lock = true;
+
+        setTimeout(() =>{
+            gameOver = false;
+            contatore = 0;
+        }, 10000)
     } else {
         contatore++;
+        button.innerHTML = `#${contatore}`;
     }
     button.innerHTML = `#${contatore}`;
 })
