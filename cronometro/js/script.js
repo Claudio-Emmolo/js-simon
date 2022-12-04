651d9f449ed1730472c31c252ad84a80871a835c
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-12-04 18:00:13 +0100

    Add Sec Min Hours

--- cronometro/js/script.js
+++ cronometro/js/script.js
@@ -1,13 +1,26 @@
-const tempo = document.getElementById('tempo');
+const secEle = document.getElementById('sec');
+const minEle = document.getElementById('min');
+const hoursEle = document.getElementById('hours');
+
+
 const start = document.getElementById('start');
 const pause = document.getElementById('pause');
 const reset = document.getElementById('reset');
 
-let time = 0;
+let sec = 0;
+
+let min = 0;
+
+let hours = 0;
 
 let = startClicked = true;
 
-tempo.innerHTML = "00"
+secEle.innerHTML = "00"
+
+minEle.innerHTML = "00"
+
+hoursEle.innerHTML = "00"
+
 
 let timer;
 
@@ -17,9 +30,26 @@ start.addEventListener('click', function(){
         startClicked = false;
     timer = setInterval(function(){
         startClicked = false;
-        time++;
-        tempo.innerHTML = time
-    }, 100);
+        sec++;
+        secEle.innerHTML = sec;
+        addZeroBefore(sec, secEle);
+
+        if (sec === 60){
+            sec = 0;
+            min += 1;
+            minEle.innerHTML = min;
+            addZeroBefore(min, minEle);
+        }
+
+        if (min === 60){
+            min = 0;
+            hours += 1;
+            hoursEle.innerHTML = hours;
+            addZeroBefore(hours, hoursEle);
+        }
+        
+    }, 1000);
+
     }
 })
 
@@ -30,6 +60,21 @@ pause.addEventListener('click', function(){
 })
 
 reset.addEventListener('click', function(){
-    time = 0;
-    tempo.innerHTML = "00";
+    sec = 0;
+    min = 0;
+    hours = 0;
+    secEle.innerHTML = "00";
+    minEle.innerHTML = "00";
+    hoursEle.innerHTML = "00";
+
 })
+
+
+
+// --------FUNCTIONS----------
+
+function addZeroBefore(time, timeElement) {
+    if (time < 10){
+        timeElement.innerHTML = "0"+time;
+    }
+}
